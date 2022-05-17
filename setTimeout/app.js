1eeed02b5482ee57a19a97a2925dc913b8432c31
Hassan El Abdallah <74018203+hsnkh12@users.noreply.github.com>
2022-05-17 22:32:19 +0300

    Add files via upload

--- setTimeout/app.js
+++ setTimeout/app.js
@@ -19,15 +19,19 @@ call1()
 setTimeout(callback1, 1000) 
 setTimeout(callback2, 0) 
 call4()
-// 1- "Call stack" calls 1
-// 2- Callback added to -> (Callback queue)
-// 3- "Call stack" calls 3
-// 4- "Call stack" completed all calls
-// 5- (Callback queue) added to -> "Call stack"
-// 6- "Call stack" calls 2
+// 1- "Call stack" calls '1'
+// 2- Callback1 added to -> (Callback aueue)
+// 3- Callback2 added to -> (Callback queue)
+// 4- "Call stack" calls '4'
+// 5- "Call stack" completed all calls
+// 6- (Callback queue) added to -> "Call stack"
+// 7- "Call stack" calls '3' - 0 msec
+// 8- "Call stack" calls '2'- 1000 msec
+// 9- "Call stack' completed all calls
 
 /*
     Call 1
+    Call 4
     Call 3
     Call 2
  */
